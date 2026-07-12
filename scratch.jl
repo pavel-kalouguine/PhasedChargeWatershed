ab88467237097c57c6793e4e2312f6d86922742b
Pavel Kalugin <pkfrance@yahoo.com>
2026-07-12 21:55:33 +0200

    Add synthetic data 2D

--- scratch.jl
+++ scratch.jl
@@ -17,12 +17,18 @@ function load_data(file_path::String)
     sf = Vector{Complex{Float64}}(undef, length(data["reflections"]))
     for (i, r) in enumerate(data["reflections"])
         k = SVector{length(r["k"]),Int}(r["k"])
-        add_peak!(dd, k, Float64(r["I"]))
+        orbit_length=add_peak!(dd, k, Float64(r["I"]))
+        if orbit_length==0
+            throw(ArgumentError("Reflection $k is already accounted for in the diffraction data"))
+        end
         sf[i] = Complex(r["ampl"][1], r["ampl"][2])
     end
+    # TODO: add a sanity check that the phases of structure factors of orbits of the real type are 
+    # consistent with the symmetry of the space group. Attention: such structure factors are 
+    # not necessarity real numbers!
 
     dd, sf
 end
 
-filepath = joinpath(@__DIR__, "data", "CdYb.json")
+filepath = joinpath(@__DIR__, "data", "synthetic.json")
 dd, sf = load_data(filepath)
